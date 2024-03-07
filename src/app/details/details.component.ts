dd3a10018e059504a738e6ab13d39ccce2ed22e9
Sean Ebenmelu <sce22@calvin.edu>
2024-03-07 10:26:11 -0500

    finished Angular Tutorial

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -56,8 +56,11 @@ export class DetailsComponent {
   })
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10)
+
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    })
   }
 
   submitApplication() {
